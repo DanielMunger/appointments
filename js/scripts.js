70d2e44b07bebb90c9888092d37f013d65221c1d
daniel munger and taylor loftis-kim <daniel@>
2016-10-19 13:28:15 -0700

    create a functioning form sheet that returns submitted information.

--- js/scripts.js
+++ js/scripts.js
@@ -2,7 +2,22 @@ $(document).ready(function(){
   $("form#name").submit(function(event){
     event.preventDefault();
     var name1 = $("#name1").val();
-    alert(name1);
-    $(".output").text(name1);
+    $("#output-name").text(name1);
+  });
+  $("form#date").submit(function(event){
+    event.preventDefault();
+    var date1 = $("#date1").val();
+    $("#output-date").text(date1);
+  });
+  $("form#start-time").submit(function(event){
+    event.preventDefault();
+    var start = $("#start").val();
+    $("#output-start").text(start);
+  });
+  $("form#end-time").submit(function(event){
+    event.preventDefault();
+    var end = $("#end").val();
+    $("#output-end").text(end);
+    alert("You have successfully made an appointment with me!")
   });
 });
